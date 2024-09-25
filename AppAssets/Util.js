b3e8a5c1f6a6ab657846a94c2e1ccf3809b087c5
Forbirdden <102984166+Forbirdden@users.noreply.github.com>
2024-09-25 19:57:07 +0500

    Add files via upload

--- AppAssets/Util.js
+++ AppAssets/Util.js
@@ -14,34 +14,25 @@ module.exports = class Util {
 				Constants.Badges.BOT_SLASH,
 				Constants.Badges.BOT_AUTOMOD,
 			);
-		// Check Nitro Type
-		if (userData.premium_type > 0) {
-			badges.push(Constants.Badges.NITRO);
-			if (userData.premium_type == 2) {
-				badges.push(Constants.Badges.GUILD_BOOSTER);
-			}
+		if (userData.avatar?.includes('a_') || userData.banner) {
+			badges.push(Constants.Badges.NITRO, Constants.Badges.GUILD_BOOSTER);
 		}
-		// Old username
 		if (userData.discriminator === '0')
 			badges.push(Constants.Badges.LEGACY_USERNAME);
 		return {
 			user: userData,
 			connected_accounts: [],
 			premium_since: null,
-			// Force enable Nitro features (Bot)
-			premium_type: userData.bot ? 2 : userData.premium_type,
+			premium_type: userData.banner ? 2 : null,
 			premium_guild_since: null,
 			profile_themes_experiment_bucket: 4,
 			mutual_guilds: [],
-			mutual_friends: [],
-			mutual_friends_count: 0,
+			mutual_friends_count: 10,
 			application_role_connections: [],
 			user_profile: {
 				bio: userData.bio,
 				accent_color: userData.accent_color,
 				banner: userData.banner,
-				pronouns: userData.pronouns,
-				profile_effect: null,
 				theme_colors: null,
 				popout_animation_particle_type: null,
 				emoji: null,
@@ -51,11 +42,54 @@ module.exports = class Util {
 			legacy_username: null,
 		};
 	}
+	static patchCommand(command) {
+		return {
+			...('metadata' in command ? command.metadata : command),
+			id: SnowflakeUtil.generate(),
+			application_id: '1056491867375673424',
+			version: SnowflakeUtil.generate(),
+		};
+	}
 	static getIDFromToken(token) {
 		token = token.replace(/^(Bot|Bearer)\s*/i, '');
 		return Buffer.from(token.split('.')[0], 'base64').toString();
 	}
 	static UserAgent() {
 		return `DiscordBot (https://github.com/aiko-chan-ai/DiscordBotClient, v${version})`;
+	}
+	static createMessageReplyCommand(
+		content,
+		applicationId,
+		interactionId,
+		channelId,
+		commandName,
+		botId,
+		nonce,
+	) {
+		return {
+			applicationId,
+			author: {
+				id: applicationId,
+			},
+			channel_id: channelId,
+			content,
+			embeds: [],
+			flags: 1 << 6,
+			id: SnowflakeUtil.generate(),
+			interaction: {
+				id: interactionId,
+				name: commandName,
+				type: 2,
+				user: {
+					id: botId,
+					username: 'BotClient',
+					avatar: null,
+				},
+				displayName: commandName,
+			},
+			timestamp: new Date().toISOString(),
+			type: 20, // Chat Input
+			nonce,
+		};
 	}
 };
